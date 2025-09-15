857dc1d9c897ee0013d1643ed7ea065111d6cd9a
Khireec <khireecarr479@gmail.com>
2025-09-15 15:21:54 -0400

    Added missing test file

---
+++ UUIDTest.cs
@@ -0,0 +1,20 @@
+﻿using Xunit;
+using PasswordStrength;
+
+namespace UUIDtest;
+
+public class UuidTests
+
+{
+    [Fact]
+    public void TestGenerateUUIDv4()
+    {
+        string uuid = Uuid.GenerateUUIDv4();
+
+
+        Assert.True(System.Guid.TryParse(uuid, out var parsedGuid));
+
+
+        Assert.Equal('4', uuid[14]);
+    }
+ }
